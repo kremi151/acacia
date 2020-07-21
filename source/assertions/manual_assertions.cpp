ba2df3d4fe48edabc6e0bcc41f8595adf4a18608
Michel Kremer <kremi151dev@gmail.com>
2020-07-21 22:18:14 +0200

    Move implementation for failure to separate cpp file

---
+++ source/assertions/manual_assertions.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Michel on 21.07.2020.
+//
+
+#include "manual_assertions.h"
+
+#include <registry/registry.h>
+#include <exceptions/assertion_exception.h>
+
+void __acacia_fail(const std::string& message, const char *file, unsigned int line) {
+    throw acacia::AssertionException(file, acacia::Registry::instance().currentTest().testName, line, message);
+}
--- source/assertions/manual_assertions.h
+++ source/assertions/manual_assertions.h
@@ -5,15 +5,11 @@
 #ifndef ACACIA_MANUAL_ASSERTIONS_H
 #define ACACIA_MANUAL_ASSERTIONS_H
 
-#include <registry/registry.h>
-#include <exceptions/assertion_exception.h>
 #include <string>
 
-void __acacia_fail(const std::string& message, const char *file, unsigned int line) {
-    throw acacia::AssertionException(file, acacia::Registry::instance().currentTest().testName, line, message);
-}
+void __acacia_fail(const std::string& message, const char *file, unsigned int line);
 
-#define failure(message) \
+#define testFailure(message) \
 __acacia_fail(message, __FILE__, __LINE__)
 
 #endif //ACACIA_MANUAL_ASSERTIONS_H
