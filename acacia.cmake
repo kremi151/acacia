5ce2ebc4fe646c264cbbdfbfc4cd00089f27291a
Michel Kremer <kremi151dev@gmail.com>
2021-05-13 12:34:27 +0200

    Native tool for ahead of time code generation (#9)
    * [WIP] acacia-gen implementation
    
    * Append empty string to custom command dependencies to work around linker issue
    
    * Improved logging
    
    * Code improvements

--- acacia.cmake
+++ acacia.cmake
@@ -11,32 +11,17 @@ endmacro()
 
 macro(set_acacia_test_sources)
     set_acacia_paths_internal()
+    file(MAKE_DIRECTORY ${ACACIA_GENERATED_DIR})
+
     set(ACACIA_TEST_SOURCES ${ACACIA_TEST_SOURCES} ${ARGV})
-    set(ACACIA_SUITE_IMPORTS "")
-    set(ACACIA_REPORT_EXECUTION_CODE "")
-    foreach(ACACIA_TEST_SOURCE ${ARGV})
-        message(STATUS "Test source: ${ACACIA_TEST_SOURCE}")
 
-        file(READ ${ACACIA_TEST_SOURCE} ACACIA_TEST_SOURCE_CONTENT)
-        string(REGEX MATCH "TEST_SUITE\\s*\\(([^)]+)\\)" _ ${ACACIA_TEST_SOURCE_CONTENT})
+    add_custom_command(
+            OUTPUT ${ACACIA_GENERATED_DIR}/suites.h ${ACACIA_GENERATED_DIR}/test_suites.cpp
+            COMMAND acacia-gen ${CMAKE_CURRENT_SOURCE_DIR} ${ACACIA_GENERATED_DIR} ${ARGV}
+            DEPENDS acacia-gen
+            MAIN_DEPENDENCY ${ARGV} ""
+    )
 
-        set(ACACIA_TEST_SUITE_NAME ${CMAKE_MATCH_1})
-
-        if(NOT "${ACACIA_TEST_SUITE_NAME}" STREQUAL "")
-            message(STATUS "Found test suite '${ACACIA_TEST_SUITE_NAME}'")
-
-            configure_file("${ACACIA_SOURCE_DIR}/source/dynamic/test_suite_header.h.in" "${ACACIA_GENERATED_DIR}/suite_${ACACIA_TEST_SUITE_NAME}.h" @ONLY)
-            set(ACACIA_TEST_HEADERS ${ACACIA_TEST_HEADERS} "${ACACIA_GENERATED_DIR}/suite_${ACACIA_TEST_SUITE_NAME}.h")
-
-            set(ACACIA_SUITE_IMPORTS "${ACACIA_SUITE_IMPORTS}\n\
-#include \"suite_${ACACIA_TEST_SUITE_NAME}.h\"")
-            set(ACACIA_SUITE_MAP_CODE "${ACACIA_SUITE_MAP_CODE}\n\
-suites[\"${ACACIA_TEST_SUITE_NAME}\"] = __Acacia__TestSuite_${ACACIA_TEST_SUITE_NAME}::runSuite;")
-        endif()
-    endforeach()
-
-    configure_file("${ACACIA_SOURCE_DIR}/source/dynamic/test_suites.cpp.in" "${ACACIA_GENERATED_DIR}/test_suites.cpp" @ONLY)
+    set(ACACIA_TEST_HEADERS ${ACACIA_TEST_HEADERS} "${ACACIA_GENERATED_DIR}/suites.h")
     set(ACACIA_TEST_SOURCES ${ACACIA_TEST_SOURCES} "${ACACIA_GENERATED_DIR}/test_suites.cpp")
-
-    set(ACACIA_TEST_SOURCE_CONTENT "")
 endmacro()
